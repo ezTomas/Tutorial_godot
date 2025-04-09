a33884e9c489395b001af8a3f39f1f7ddb60fb4d
Enzo Tomas <tomasenzo03@gmail.com>
2025-04-09 09:55:34 -0300

    Finalizando el juego

--- tutorial-de-godot/main.gd
+++ tutorial-de-godot/main.gd
@@ -8,6 +8,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	$HUD.update_score(score)
@@ -15,6 +17,7 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	get_tree().call_group("mobs", "queue_free" )
+	$Music.play()
 
 func _on_score_timer_timeout():
 	$HUD.update_score(score)
